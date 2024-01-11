d3e580098a47fb6b2445812926800e5a4b94921e
Vinay Nishad <122341170+vinaynishad45@users.noreply.github.com>
2024-01-11 10:37:19 +0000

    changes done

---
+++ 1_basic/dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all javascript code newer version
+
+// alert(10+20);
+
+console.log(5+20);
+
+// code readability should be high
+
+let name="Sandeep"
+
+let age =22
+let status=true
+// types of datatype
+
+// number
+// bigint 
+// boolean 
+// null
+// undefined
+// symbol
+
+// object
+
+console.log(typeof(null));
+
+
---
+++ 1_basic/variable.js
@@ -0,0 +1,16 @@
+var acountHolderName="Vinay Kumar Nishad";
+let email="vinay@gmail.com";
+const acountNumber="4545908765";
+
+/*
+Prefer not to use var
+Because issue in block and functional scope
+*/
+// acountNumber="2345678989";
+// console.log(acountNumber);
+let acountState;
+let acountId=1234567;
+let address="Noida Elactronic City";
+let phone=9876456789;
+console.table([acountHolderName,email,acountNumber,acountId,address,phone,acountState]);
+
